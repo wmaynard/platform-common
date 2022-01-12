ab6e819916e7e10684d628d999c7c74c8f6f534c
Sean Chapel <sean@seoushigames.com>
2022-01-12 11:05:33 -0800

    v1.0.70 removing token service and adding tokenhelper for fetching admin tokens

---
+++ Utilities/TokenHelper.cs
@@ -0,0 +1,30 @@
+using Rumble.Platform.Common.Utilities;
+using Rumble.Platform.CSharp.Common.Interop;
+
+namespace Rumble.Platform.CSharp.Common.Utilities
+{
+    public class TokenHelper
+    {
+        public static string GetAdminToken()
+        {
+            DynamicConfigClient client = ServicesManager.Get<DynamicConfigClient>();
+
+            if (client == null)
+            {
+                Log.Error(Owner.Sean, "DynamicConfigClient needs to be started before requesting the admin token");
+                return null;
+            }
+            
+            string variableName = PlatformEnvironment.Variable("RUMBLE_COMPONENT") + "-token";
+
+            string token = client.GetGameConfig(variableName);
+
+            if (string.IsNullOrEmpty(token))
+            {
+                Log.Error(Owner.Sean, "Missing admin token for service");
+            }
+
+            return token;
+        }
+    }
+}
