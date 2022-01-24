dc7e7e8f736454fcedc448539ee6fad0c9398b0c
Sean Chapel <sean@seoushigames.com>
2022-01-24 13:12:46 -0800

    1.0.82 - adding check for if dynamic config is initialized

--- Interop/DynamicConfigClient.cs
+++ Interop/DynamicConfigClient.cs
@@ -33,6 +33,7 @@ namespace Rumble.Platform.Common.Interop
         private DateTime _lastUpdateTime;
         private Task _updateTask;
         private int _lastListenerId = 0;
+        private bool _isInitialized = false;
 
         public DynamicConfigClient(String configServiceUrl, String secret, string gameId)
         {
@@ -294,6 +295,7 @@ namespace Rumble.Platform.Common.Interop
 
             _isUpdating = false;
             _lastUpdateTime = DateTime.Now;
+            _isInitialized = true;
         }
         
 
