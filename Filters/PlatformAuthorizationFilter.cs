fa677a40e636d314bcdc6878e6b43c15d690bcde
Will Maynard <will@willmaynard.com>
2022-04-19 14:58:52 -0700

    1.0.134 | PlatformAuthorizationFilter.Authorize() for F# services

--- Filters/PlatformAuthorizationFilter.cs
+++ Filters/PlatformAuthorizationFilter.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -14,7 +15,7 @@ namespace Rumble.Platform.Common.Filters
 {
 	public class PlatformAuthorizationFilter : PlatformBaseFilter, IAuthorizationFilter
 	{
-		private static readonly string TokenAuthEndpoint = PlatformEnvironment.TokenValidation;
+		private const int TOKEN_CACHE_EXPIRATION = 600_000; // 10 minutes
 		public const string KEY_TOKEN = "PlatformToken";
 		
 		/// <summary>
@@ -61,7 +62,7 @@ namespace Rumble.Platform.Common.Filters
 						string message = response.OriginalResponse.Optional<string>("message") ?? "no message provided";
 						string eventId = response.OriginalResponse.Optional<string>("eventId");
 						
-						errorMessage = $"Token auth failure: {message}";
+						errorMessage = $"Token encryptedToken failure: {message}";
 						Log.Error(Owner.Default, errorMessage, data: new
 						{
 							ValidationUrl = PlatformEnvironment.TokenValidation,
@@ -80,7 +81,7 @@ namespace Rumble.Platform.Common.Filters
 					.OnSuccess((sender, response) =>
 					{
 						tokenInfo = response.AsGenericData.Require<TokenInfo>("tokenInfo");
-						_cacheService?.Store(bearerToken, tokenInfo, expirationMS: 600_000); // 10 minutes
+						_cacheService?.Store(bearerToken, tokenInfo, expirationMS: TOKEN_CACHE_EXPIRATION);
 						Graphite.Track(
 							name: Graphite.KEY_AUTHORIZATION_COUNT,
 							value: 1,
@@ -103,7 +104,58 @@ namespace Rumble.Platform.Common.Filters
 					data: new PlatformException(errorMessage, code: ErrorCode.TokenValidationFailed)
 				));
 		}
-		
+
+		/// <summary>
+		/// This method is used for F# token validation.  F# can't make use of the filters appropriately, so this is an effective workaround.
+		/// It should not be used in C# services, however.
+		/// </summary>
+		/// <param name="encryptedToken">The token from the Authorization Header.  It may either include or omit "Bearer ".</param>
+		/// <returns>Decrypted TokenInfo.</returns>
+		public static TokenInfo Validate(string encryptedToken)
+		{
+			encryptedToken = encryptedToken?.Replace("Bearer ", "");
+			
+			ApiService api = GetService<ApiService>();
+			CacheService cache = GetService<CacheService>();
+
+			TokenInfo output;
+			if (cache.HasValue(encryptedToken, out output))
+				return output;
+
+			// If a token is provided and does not exist in the cache, we should validate it.
+			// TODO: This is mostly copyapasta from the event, so it's a little WET.
+			if (!string.IsNullOrWhiteSpace(encryptedToken))
+				api
+					.Request(PlatformEnvironment.TokenValidation)
+					.AddAuthorization(encryptedToken)
+					.OnFailure((sender, response) =>
+					{
+						string message = response.OriginalResponse.Optional<string>("message") ?? "no message provided";
+						string eventId = response.OriginalResponse.Optional<string>("eventId");
+						
+						string errorMessage = $"Token auth failure: {message}";
+						Log.Error(Owner.Default, errorMessage, data: new
+						{
+							ValidationUrl = PlatformEnvironment.TokenValidation,
+							Code = response.StatusCode,
+							EncryptedToken = encryptedToken,
+							EventId = eventId
+						});
+					})
+					.OnSuccess((sender, response) =>
+					{
+						output = response.AsGenericData.Require<TokenInfo>("tokenInfo");
+						cache?.Store(encryptedToken, output, expirationMS: TOKEN_CACHE_EXPIRATION);
+						
+						HttpContext context = new HttpContextAccessor()?.HttpContext;
+						if (context != null)
+							context.Items[KEY_TOKEN] = output;
+					})
+					.Get();
+
+			return output;
+		}
+
 		private static TokenInfo AddToContext(ref FilterContext context, TokenInfo info)
 		{
 			if (context != null)
