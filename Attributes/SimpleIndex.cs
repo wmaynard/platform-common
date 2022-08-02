b9d553cb7324d434104b61d40321f189ec85e4b4
Will Maynard <will@willmaynard.com>
2022-08-02 16:37:22 -0700

    2 space tabs -> 4 space tabs

--- Attributes/SimpleIndex.cs
+++ Attributes/SimpleIndex.cs
@@ -5,22 +5,22 @@ namespace Rumble.Platform.Common.Attributes;
 [AttributeUsage(validOn: AttributeTargets.Property)]
 public class SimpleIndex : Attribute
 {
-  public string DatabaseKey { get; init; }
-  public string Name { get; init; }
-  public string PropertyName { get; private set; }
+    public string DatabaseKey { get; init; }
+    public string Name { get; init; }
+    public string PropertyName { get; private set; }
   
-  public bool Unique { get; init; }
+    public bool Unique { get; init; }
 
-  public SimpleIndex(string dbKey, string name = null, bool unique = false)
-  {
-    DatabaseKey = dbKey;
-    Name = name ?? dbKey;
-    Unique = unique;
-  }
+    public SimpleIndex(string dbKey, string name = null, bool unique = false)
+    {
+        DatabaseKey = dbKey;
+        Name = name ?? dbKey;
+        Unique = unique;
+    }
 
-  internal SimpleIndex SetPropertyName(string name)
-  {
-    PropertyName = name;
-    return this;
-  }
+    internal SimpleIndex SetPropertyName(string name)
+    {
+        PropertyName = name;
+        return this;
+    }
 }
