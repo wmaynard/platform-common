1f2e2d9a13a97af267100bfb517b5f5f9451eadb
Will Maynard <will@willmaynard.com>
2023-02-06 02:45:49 -0800

    1.3.7 | Add DC diff support methods

--- Models/Config/Section.cs
+++ Models/Config/Section.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
@@ -77,4 +78,149 @@ public class Section : PlatformCollectionDocument
         ActiveClients = new List<DynamicConfig.DC2ClientInformation>();
         return this;
     }
+
+    [BsonIgnore]
+    [JsonIgnore]
+    public string[] AllKeys => Data
+        .Select(json => $"{Name}.{json.Key}")
+        .ToArray();
+
+    public static Dictionary<string, string> Flatten(IEnumerable<Section> sections)
+    {
+        var x  = sections
+            .Select(section => section.Data
+                .ToDictionary(
+                    keySelector: pair => $"{section.Name}.{pair.Key}",
+                    elementSelector: pair => pair.Value.Value
+                )
+            );
+
+        Dictionary<string, string> output = new Dictionary<string, string>();
+        foreach (Dictionary<string, string> dict in x)
+        {
+            foreach (string key in dict.Keys)
+                output[key] = dict[key];
+        }
+
+        return output;
+    }
+
+    /// <summary>
+    /// Flattens a config into some simple tuples for easy Diff manipulation.
+    /// Item1: The environment the config is for.
+    /// Item2: {service name}.{key}
+    /// Item3: {value}
+    /// </summary>
+    /// <param name="environment"></param>
+    /// <param name="sections"></param>
+    /// <returns></returns>
+    private static List<Tuple<string, string, string>> Flatten(string environment, IEnumerable<Section> sections)
+    {
+        var bar = sections.First().Data.Select(setting => new Tuple<string, string, string>(
+            item1: environment,
+            item2: $"blah.{setting.Key}",
+            item3: setting.Value.Value
+        ));
+        
+        var output = sections
+            .SelectMany(section => section.Data.Select(setting => new Tuple<string, string, string>(
+                item1: environment,
+                item2: $"{section.Name}.{setting.Key}",
+                item3: setting.Value.Value
+            )))
+            .ToList();
+
+        return output;
+    }
+
+    /// <summary>
+    /// Turn tuples into an array of Diffs.  This filters out items that are
+    /// </summary>
+    /// <param name="urls"></param>
+    /// <param name="list"></param>
+    /// <returns></returns>
+    private static Diff[] DiffsFromTuples(List<string> urls, List<Tuple<string, string, string>> list)
+    {
+        list = list.OrderBy(tuple => tuple.Item2).ToList();
+        string[] allKeys = list.Select(tuple => tuple.Item2).Distinct().ToArray();
+        
+        foreach (string key in allKeys)
+        {
+            string[] missing = urls
+                .Except(list
+                    .Where(tuple => tuple.Item2 == key)
+                    .Select(tuple => tuple.Item1)
+                )
+                .ToArray();
+            bool uniform = list
+                .Where(tuple => tuple.Item2 == key)
+                .Select(tuple => tuple.Item3)
+                .Distinct()
+                .Count() == 1;
+
+            if (missing.Any())
+                list.AddRange(missing.Select(str => new Tuple<string, string, string>(
+                    item1: str,
+                    item2: key,
+                    item3: "THIS VALUE IS MISSING."
+                )));
+            else if (uniform)
+                list.RemoveAll(tuple => tuple.Item2 == key);
+        }
+
+        return list
+            .GroupBy(tuple => tuple.Item2)
+            .Select(group => new Diff
+            {
+                Key = group.Key,
+                Data = group
+                    .Where(tuple => !tuple.Item3.EndsWith(".adminToken")) // Omit admin tokens for security reasons / unnecessary.
+                    .Select(tuple => new DiffValue
+                    {
+                        Environment = tuple.Item1,
+                        Value = tuple.Item3
+                    })
+                    .ToArray()
+            })
+            .ToArray();
+    }
+
+    /// <summary>
+    /// Takes a dictionary of environment / sections and returns a diff of all of them.  The dictionary keys
+    /// are environment URLs; the values array is the entire config for that environment.
+    /// The returned data is sorted by {service-name}.{config key}, ascending.  Items that are uniform and present
+    /// on all specified environments are omitted.
+    /// </summary>
+    /// <param name="dict"></param>
+    /// <param name="limiter">Adds a service filter, if desired, to return a partial diff.</param>
+    /// <returns></returns>
+    public static Diff[] GetDiff(Dictionary<string, Section[]> dict, string limiter = null)
+    {
+        List<Tuple<string, string, string>> flattened = new List<Tuple<string, string, string>>();
+        
+        foreach (KeyValuePair<string, Section[]> pair in dict)
+            flattened.AddRange(Flatten(pair.Key, pair.Value));
+
+        Diff[] output = DiffsFromTuples(dict.Keys.ToList(), flattened);
+
+        return limiter == null
+            ? output
+            : output.Where(diff => diff.Key.StartsWith(limiter)).ToArray();
+
+    }
+    
+    public class Diff : PlatformDataModel
+    {
+        public string Key { get; set; }
+        public DiffValue[] Data { get; set; }
+    }
+    
+    public class DiffValue : PlatformDataModel
+    {
+        public string Environment { get; set; }
+        public string Value { get; set; }
+    }
 }
+
+
+
