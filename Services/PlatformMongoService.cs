a3ebf6d0bcd5520d5ba572408c270c58265ef1be
Will Maynard <will@willmaynard.com>
2024-05-15 16:46:10 -0700

    1.3.153 | Revert select changes from 1.3.150
    PlatformMongoService changed the way null IDs were managed, which caused problems in nft-service

--- Services/PlatformMongoService.cs
+++ Services/PlatformMongoService.cs
@@ -182,11 +182,10 @@ public abstract class PlatformMongoService<Model> : PlatformService, IPlatformMo
         if (!createIfNotFound && model.Id == null)
             throw new PlatformException(message: "Model.Id is null; update will be unsuccessful without upsert.");
         ReplaceOptions options = new() { IsUpsert = createIfNotFound };
-        string id = model.Id ?? ObjectId.GenerateNewId().ToString();
         if (session != null)
-            _collection.ReplaceOne(session, filter: m => model.Id == id, replacement: model, options: options);
+            _collection.ReplaceOne(session, filter: m => model.Id == m.Id, replacement: model, options: options);
         else
-            _collection.ReplaceOne(filter: m => model.Id == id, replacement: model, options: options);
+            _collection.ReplaceOne(filter: m => model.Id == m.Id, replacement: model, options: options);
     }
 
     public virtual Model[] Find(Expression<Func<Model, bool>> filter) => _collection.Find(filter).ToList().ToArray();
